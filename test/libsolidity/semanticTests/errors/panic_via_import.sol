9187f4f4b0edf7a3bfac61cd7ae3d6fbfeb9fe9e
chriseth <chris@ethereum.org>
2021-03-25 11:51:43 +0100

    fixup! Revert statement.

--- test/libsolidity/semanticTests/errors/panic_via_import.sol
+++ test/libsolidity/semanticTests/errors/panic_via_import.sol
@@ -3,11 +3,16 @@ error E(uint);
 ==== Source: s2.sol ====
 import { E as Panic } from "s1.sol";
 contract C {
-  function a() public pure {
-    revert Panic(1);
-  }
+    error E(uint);
+    function a() public pure {
+        revert Panic(1);
+    }
+    function b() public pure {
+        revert E(1);
+    }
 }
 // ====
 // compileViaYul: also
 // ----
 // a() -> FAILURE, hex"002ff067", hex"0000000000000000000000000000000000000000000000000000000000000001"
+// b() -> FAILURE, hex"002ff067", hex"0000000000000000000000000000000000000000000000000000000000000001"
--- test/libsolidity/semanticTests/errors/using_structs.sol
+++ test/libsolidity/semanticTests/errors/using_structs.sol
@@ -1,12 +1,16 @@
 pragma abicoder v2;
 struct S { uint a; string b; }
-error E(uint a, S, uint b);
+error E(uint a, S s, uint b);
 contract C {
     S s;
     function f(bool c) public {
-        s.a = 9;
-        s.b = "abc";
-        revert E(2, s, 7);
+        if (c) {
+            s.a = 9;
+            s.b = "abc";
+            revert E(2, s, 7);
+        } else {
+            revert E({b: 7, a: 2, s: S({b: "abc", a: 9})});
+        }
     }
 }
 // ====
